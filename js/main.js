f44ba80ff86cdb2af933c5b64be58d3557701ae3
Alex Alex <webdevalex17@gmail.com>
2023-04-21 01:54:28 +0300

    new: added next section

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
-$(function(){
+"use strict";
 
-});
+const tabItem = document.querySelectorAll(".tabs__btn-item");
+const tabContent = document.querySelectorAll(".tabs__content-item");
+
+tabItem.forEach((item) => item.addEventListener("click", open));
+
+function open(e) {
+  const tabTarget = e.currenttarget;
+  const button = tabTarget.dataset.button;
+
+  tabItem.forEach((item) => item.classList.remove("tabs__btn-item--active"));
+
+  tabContent.forEach((item) =>
+    item.classList.remove("tabs__content-item--active")
+  );
+  tabTarget.classList.add("tabs__btn-item--active");
+  document
+    .querySelector(`#${button}`)
+    .classList.add("tabs__content-item--active");
+}
